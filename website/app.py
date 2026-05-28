76b6a1fc84be415166869357bbc1f983b91ff2d1
Bless2718 <Blesson2718@gmail.com>
2026-05-29 01:11:52 +0530

    Fix Render port binding

--- website/app.py
+++ website/app.py
@@ -258,12 +258,12 @@ with app.app_context():
 
     db.create_all()
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(
-
-        debug=True,
-
-        host="127.0.0.1",
-
-        port=5000
+        host="0.0.0.0",
+        port=port,
+        debug=True
     )
